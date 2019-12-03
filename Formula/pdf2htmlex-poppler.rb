b570670141a18dafc6771c6f5bfb28110e54546a
Stephen Gaito <stephen@perceptisys.co.uk>
2019-12-03 12:31:43 +0000

    initial creation of pdf2htmlEX specific homebrew formulae

---
+++ Formula/pdf2htmlex-poppler.rb
@@ -0,0 +1,86 @@
+# Taken from 
+# https://github.com/Homebrew/linuxbrew-core/raw/9ba1f92205050b015188c441b9b315a3aca70ff6/Formula/poppler.rb
+# on 2019-12-03 by Stephen Gaito as part of the upgrade of the pdf2htmlEX formula
+
+class Pdf2htmlexPoppler < Formula
+  desc "PDF rendering library (based on the xpdf-3.0 code base)"
+  homepage "https://poppler.freedesktop.org/"
+  url "https://poppler.freedesktop.org/poppler-0.81.0.tar.xz"
+  sha256 "212d020b035b67b36c9666bd08ac778dff3222d85c01c338787d546f0f9bfe02"
+  head "https://anongit.freedesktop.org/git/poppler/poppler.git"
+
+  bottle do
+    sha256 "9b97533f63d2e09d7fdbeeff064015d43d7490cad118ca559d8c699985e9778c" => :catalina
+    sha256 "c41c9aff6ed97d122c0806da50ed6ec8d9e7df118c38379530bf72850979927e" => :mojave
+    sha256 "b55894e5ad440cd064a7dbac82510d91041d188501a9659f4ba76bef13acbe6d" => :high_sierra
+    sha256 "3808c041f0ca08e9df0b83a65d78ac4680645d51bbbb778ecb5a4162f271a90f" => :x86_64_linux
+  end
+
+  depends_on "cmake" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "little-cms2"
+  depends_on "nss"
+  depends_on "openjpeg"
+  depends_on "qt"
+  uses_from_macos "curl"
+
+  conflicts_with "pdftohtml", "pdf2image", "xpdf",
+    :because => "poppler, pdftohtml, pdf2image, and xpdf install conflicting executables"
+
+  resource "font-data" do
+    url "https://poppler.freedesktop.org/poppler-data-0.4.9.tar.gz"
+    sha256 "1f9c7e7de9ecd0db6ab287349e31bf815ca108a5a175cf906a90163bdbe32012"
+  end
+
+  def install
+    ENV.cxx11
+
+    args = std_cmake_args + %w[
+      -DBUILD_GTK_TESTS=OFF
+      -DENABLE_CMS=lcms2
+      -DENABLE_GLIB=ON
+      -DENABLE_QT5=ON
+      -DENABLE_UNSTABLE_API_ABI_HEADERS=ON
+      -DWITH_GObjectIntrospection=ON
+    ]
+
+    system "cmake", ".", *args
+    system "make", "install"
+    system "make", "clean"
+    system "cmake", ".", "-DBUILD_SHARED_LIBS=OFF", *args
+    system "make"
+    lib.install "libpoppler.a"
+    lib.install "cpp/libpoppler-cpp.a"
+    lib.install "glib/libpoppler-glib.a"
+    resource("font-data").stage do
+      system "make", "install", "prefix=#{prefix}"
+    end
+
+    if OS.mac?
+      libpoppler = (lib/"libpoppler.dylib").readlink
+      [
+        "#{lib}/libpoppler-cpp.dylib",
+        "#{lib}/libpoppler-glib.dylib",
+        "#{lib}/libpoppler-qt5.dylib",
+        *Dir["#{bin}/*"],
+      ].each do |f|
+        macho = MachO.open(f)
+        macho.change_dylib("@rpath/#{libpoppler}", "#{lib}/#{libpoppler}")
+        macho.write!
+      end
+    end
+  end
+
+  test do
+    system "#{bin}/pdfinfo", test_fixtures("test.pdf")
+  end
+end
