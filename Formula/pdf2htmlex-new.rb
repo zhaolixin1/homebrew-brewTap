b7cac45c0412b81df806053c988f3c8ff24d1b06
Stephen Gaito <stephen@perceptisys.co.uk>
2019-12-12 06:23:14 +0000

    updated formula

--- Formula/pdf2htmlex-new.rb
+++ Formula/pdf2htmlex-new.rb
@@ -1,8 +1,8 @@
 class Pdf2htmlexNew < Formula
   desc "PDF to HTML converter"
   homepage "https://github.com/pdf2htmlEX/pdf2htmlEX/"
-  url "https://github.com/stephengaito/pdf2htmlEX/archive/newBuildSystem.tar.gz"
-  version "newBuildSystem"
+  url "https://github.com/stephengaito/pdf2htmlEX/archive/fontforge-update.tar.gz"
+  version "fontforge-update"
   sha256 "19d4bbd2368a12ee550334f1edc7c58d383aed7c3cdd2fc024bcdeabe8ae92ee"
 
   bottle do
@@ -13,8 +13,10 @@ class Pdf2htmlexNew < Formula
   depends_on "gcc" => :build unless OS.mac?
   depends_on "llvm" => :build if OS.mac?
   depends_on "gnu-getopt"
-  depends_on "fontforge@20170731"
-  depends_on "poppler@0.81.0"
+#  depends_on "fontforge@20170731"
+#  depends_on "poppler@0.81.0"
+  depends_on "fontforge"
+  depends_on "poppler"
   depends_on "ttfautohint"
 
   def install
