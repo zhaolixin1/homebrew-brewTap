f5ce703e002b6ed33d3b08592f7025e29c26ef84
Stephen Gaito <stephen@perceptisys.co.uk>
2019-12-13 13:46:42 +0000

    updated pdf2htmlEX formula

--- Formula/pdf2htmlex-new.rb
+++ Formula/pdf2htmlex-new.rb
@@ -1,9 +1,9 @@
 class Pdf2htmlexNew < Formula
   desc "PDF to HTML converter"
   homepage "https://github.com/pdf2htmlEX/pdf2htmlEX/"
-  url "https://github.com/stephengaito/pdf2htmlEX/archive/fontforge-update.tar.gz"
-  version "fontforge-update"
-  sha256 "7df9821bb2bb0d07ad2e3ccfbe59ab721c8146f029648fa06d5b9bc569f2bd94"
+  url "https://github.com/stephengaito/pdf2htmlEX/archive/update-poppler.tar.gz"
+  version "update-poppler"
+  sha256 ""
 
   bottle do
   end
