5f64cbf7cabe5b4df3bc803bd840654fbecc4ac2
Stephen Gaito <stephen@perceptisys.co.uk>
2019-12-16 16:03:37 +0000

    updated old pdf2htmlEX build formula to use 20190801 and poppler-0.83.0

--- Formula/pdf2htmlex-new.rb
+++ Formula/pdf2htmlex-new.rb
@@ -2,44 +2,104 @@ class Pdf2htmlexNew < Formula
   desc "PDF to HTML converter"
   homepage "https://github.com/pdf2htmlEX/pdf2htmlEX/"
   url "https://github.com/stephengaito/pdf2htmlEX/archive/update-poppler.tar.gz"
-  version "update-poppler"
   sha256 "78ea892f7692ded40f4474469baed27206bcee740f17e8bba49f187b02a9d11e"
+  version "update-poppler"
 
   bottle do
   end
 
+  depends_on "autoconf" => :build # for fontforge
+  depends_on "automake" => :build # for fontforge
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "gcc" => :build unless OS.mac?
-  depends_on "llvm" => :build if OS.mac?
+  depends_on "cairo" # for fontforge
+  depends_on "freetype" # for fontforge
+  depends_on "gettext" # for fontforge
+  depends_on "giflib" # for fontforge
+  depends_on "glib" # for fontforge
   depends_on "gnu-getopt"
-#  depends_on "fontforge@20170731"
-#  depends_on "poppler@0.81.0"
-  depends_on "fontforge"
-  depends_on "poppler"
-  depends_on "python"
+  depends_on "jpeg" # for fontforge
+  depends_on "libpng" # for fontforge
+  depends_on "libtiff" # for fontforge
+  depends_on "libtool" # for fontforge
+
+  depends_on "openjpeg" # for poppler
+  depends_on "pango" # for fontforge
   depends_on "ttfautohint"
 
+  # We use the standard FontForge, but we need to compile it with 
+  # configuration options which are very different from those in the 
+  # standard Homebrew/Linuxbrew Formula
+  #  
+  resource "fontforge" do
+    url "https://github.com/fontforge/fontforge/releases/download/20190801/fontforge-20190801.tar.gz"
+    sha256 "d92075ca783c97dc68433b1ed629b9054a4b4c74ac64c54ced7f691540f70852"
+  end
+
+  # We also use poppler compiled with configuration options which are very 
+  # different from those in the standard Homebrew/Linuxbrew Formula
+  #
+  resource "poppler" do
+    url "https://poppler.freedesktop.org/poppler-0.83.0.tar.xz"
+    sha256 "685c650d9771e1c340ea222b57c8bd3f7502e2191ccfe8adeb9e638dcb0b799b"
+  end
+
+  resource "poppler-data" do
+    url "https://poppler.freedesktop.org/poppler-data-0.4.9.tar.gz"
+    sha256 "1f9c7e7de9ecd0db6ab287349e31bf815ca108a5a175cf906a90163bdbe32012"
+  end
+
   def install
+    resource("fontforge").stage do
+      # Fix linker error; see: https://trac.macports.org/ticket/25012
+#      ENV.append "LDFLAGS", "-lintl" if OS.mac?
 
-    # May need to explicitly:
-    #    brew reinstall -s gettext
-    # in order to ensure gettext gets built with the same libstdc++/gcc 
-    # versions
+      # Reset ARCHFLAGS to match how we build
+      ENV["ARCHFLAGS"] = "-arch #{MacOS.preferred_arch}"
 
-#    system "brew", "switch", "python", "3.7.5"
-    system "which", "python3"
-    ENV["PYTHON_CFLAGS"] = `python3-config --cflags`.chomp
-    ENV["PYTHON_LIBS"] = `python3-config --ldflags`.chomp
+      system "./autogen.sh"
+      system "./configure", "--prefix=#{libexec}/fontforge",
+                            "--without-libzmq",
+                            "--without-x",
+                            "--without-iconv",
+                            "--without-libspiro",
+                            "--without-libuninameslist",
+                            "--disable-python-scripting",
+                            "--disable-python-extension"
+      system "make"
+      system "make", "install"
+    end
 
-    Dir.chdir('pdf2htmlEX')
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{libexec}/fontforge/lib/pkgconfig"
+    ENV.prepend_path "PATH", "#{libexec}/fontforge/bin"
+
+    resource("poppler").stage do
+      inreplace "poppler.pc.in", "Cflags: -I${includedir}/poppler",
+                                 "Cflags: -I${includedir}/poppler -I${includedir}"
+
+      system "./configure", "--disable-dependency-tracking",
+                            "--prefix=#{libexec}/poppler",
+                            "--enable-xpdf-headers",
+                            "--enable-poppler-glib",
+                            "--disable-gtk-test",
+                            "--enable-introspection=no",
+                            "--disable-poppler-qt4"
+      system "make", "install"
+      resource("poppler-data").stage do
+        system "make", "install", "prefix=#{libexec}/poppler"
+      end
+    end
+
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{libexec}/poppler/lib/pkgconfig"
+    ENV.prepend_path "PATH", "#{libexec}/poppler/bin"
+
     system "cmake", ".", *std_cmake_args
     system "make"
     system "make", "install"
-    system "otool", "-L", "/usr/local/opt/fontforge/lib/libfontforge.3.dylib"
   end
 
-#  test do
-#    system "#{bin}/pdf2htmlEX", test_fixtures("test.pdf")
-#  end
+  test do
+    #system "#{bin}/pdf2htmlEX", test_fixtures("test.pdf")
+    system "#{bin}/pdf2htmlEX", "--version"
+  end
 end
